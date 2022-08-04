f6d33730e897a97ad96580c310d1d395426a0e8e
UmarRazzaq <umar1z2e3r4o@gmail.com>
2022-08-04 19:06:45 +0500

    IcA0sNXW The application should have splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Water Reminder App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
