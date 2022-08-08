d59b9b88ebf4508796914f085d54b3abd731cea0
UmarRazzaq <umar1z2e3r4o@gmail.com>
2022-08-08 14:22:41 +0500

    ixNvxvzq The application should have home and setting screen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:water_reminder/colors.dart';
 
+import 'setting_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final tabs = [
+    Scaffold(),
+    Scaffold(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+        elevation: 0.5,
+        centerTitle: true,
+        title: Text(
+          _currentIndex == 0 ? 'Home' : 'Setting',
+          style: TextStyle(color: blackColor),
+        ),
+        leading: const Text(''),
+      ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: darkWaterColor,
+        elevation: 10.0,
+        currentIndex: _currentIndex,
+        selectedLabelStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        selectedItemColor: whiteColor,
+        unselectedItemColor: greyColor,
+        unselectedLabelStyle: const TextStyle(fontFamily: 'SourceSansPro'),
+        selectedFontSize: 10.0,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:water_reminder/colors.dart';
 
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+        elevation: 0.5,
+        centerTitle: true,
+        title: Text(
+          'Setting',
+          style: TextStyle(color: blackColor),
+        ),
+        leading: const Text(''),
+      ),
+    );
+  }
+}
